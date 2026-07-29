55b07ed8b29fde9f9b2d5ffcc45ee301551911c8
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2026-07-29 10:46:27 +0800

    minor fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,15 +6,17 @@ import heroImg from "../imports/images/hero.png"
 export default function Hero() {
   return (
 
-   < section className="bg-background pt-[110px] pb-0 overflow-hidden">
+   <section className="bg-background pt-[110px] pb-0 overflow-hidden">
      
       <div className="max-w-[1240px] mx-auto px-6 md:px-12 grid md:grid-cols-2 gap-8 items-center min-h-[560px]">
       
 
       <div className="py-16">
         <span className="font-semibold text-[10px] " style={{color:"#3fbfc7"}}>Frascio Sanitary 2026 collection</span> 
-        <h1 className="font-['Space_Grotesk'] font-semibold text-[64px] md:text-[80px] leading-[1.05] tracking-[-0.02em] text-foreground mt-6 mb-7">Comfort is </h1>
-         <span className="font-semibold text-[50px]" style={{color:"#3fbfc7"}}>essential luxury.</span> 
+        <h1 className="font-['Space_Grotesk'] font-semibold text-[64px] md:text-[80px] leading-[1.05] tracking-[-0.02em] text-foreground mt-6 mb-7">Comfort is 
+           <span className="font-semibold text-[50px]" style={{color:"#3fbfc7"}}>essential luxury.</span> 
+        </h1>
+        
         <p>
           Full-dimensional intelligent control <br />
           high-efficiency water-saving flushing, <br />
