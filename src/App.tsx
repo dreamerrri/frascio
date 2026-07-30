faae01f3b0992d83fadc88d0e46afe99dadd27c0
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2026-07-30 16:41:06 +0800

    feat: add Section6-8 components and update image assets
    - Add Section6, Section7, Section8 components
    - Update App.tsx to include new sections
    - Remove postcss.config.js (recently added)
    - Swap out hero/background images (add bg7, Section6-1, Section6-2; remove unused assets)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Section2 from "./components/Section2"
 import Section3 from "./components/Section3"
 import Section4 from "./components/Section4"
 import Section5 from "./components/Section5"
+import Section6 from "./components/Section6"
+import Section7 from "./components/Section7"
+import Section8 from "./components/Section8"
 
 export default function App() {
   return (
@@ -15,13 +18,13 @@ export default function App() {
       <main>
         <Hero />
         <Section1 />
-        {/* <Features /> */}
-        {/* next section component */}
-         <Section2 />
-     
-<Section3 />
-<Section4 />
-<Section5 />
+        <Section2 />  
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Section6 />
+        <Section7 />
+        <Section8 />
       </main>
     </>
     
